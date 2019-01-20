7eedfaa81884b4134c0ca1c52d60cafde0adb902
Alexsander Vyshnyvetskyy <wdmg.com.ua@gmail.com>
2019-01-20 13:32:44 +0200

    Added base DatePicket class for widget

---
+++ src/DatePicker.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace kartik\datepicker;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\ArrayHelper;
+use yii\widgets\InputWidget;
+use yii\base\InvalidConfigException;
+
+class DatePicker extends InputWidget
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
