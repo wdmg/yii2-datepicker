afb564c9bb4e0396a34589121844fc27ecf929ed
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-20 14:20:44 +0200

    Added base functionality to display widget

--- src/DatePicker.php
+++ src/DatePicker.php
@@ -11,8 +11,39 @@ use yii\base\InvalidConfigException;
 
 class DatePicker extends InputWidget
 {
+
+    public $addon = '<span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>';
+    //public $template = '{label}\n<div class="input-group">{input}\n{addon}</div>\n{hint}\n{error}';
+    //public $template = '<div class="input-group">{input}{addon}</div>';
+    //public $template = '{beginLabel}{labelTitle}{endLabel}<div class="input-group">{input}{addon}</div>{error}{hint}';
+    public $template = '<div class="input-group">{input}{addon}</div>';
+
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         parent::init();
     }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+
+        $this->options['class'] = 'form-control';
+
+        if($this->hasModel())
+            $input = Html::activeTextInput($this->model, $this->attribute, $this->options);
+        else
+            $input = Html::textInput($this->name, $this->value, $this->options);
+
+        $this->addon = Html::tag('button', $this->addon, ['class' => 'btn btn-default', 'type' => 'button']);
+        $this->addon = Html::tag('span', $this->addon, ['class' => 'input-group-btn']);
+        $input = strtr($this->template, ['{input}' => $input, '{addon}' => $this->addon]);
+
+        echo $input;
+    }
+
 }
