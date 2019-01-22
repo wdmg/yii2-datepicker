8793a7cbbf0ad4ab217b8623384adef4e8d06d64
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-22 02:26:48 +0200

    Update composer.json and assets bundle for use remote jquery plugin

--- src/DatePickerAssets.php
+++ src/DatePickerAssets.php
@@ -6,7 +6,8 @@ use yii\web\AssetBundle;
 
 class DatePickerAssets extends AssetBundle
 {
-    public $sourcePath = '@vendor/wdmg/yii2-datepicker/src/assets';
+
+    public $sourcePath = '@bower/bootstrap-datepicker-plugin/src';
 
     public $css = [
         'css/datepicker.css',
